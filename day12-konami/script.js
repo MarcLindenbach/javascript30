d34bdd5d741cdc1f8806f3b6ebb900a408150b8e
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-12-31 07:50:23 -0700

    konami code check done using array now, added placeholder text

--- day12-konami/script.js
+++ day12-konami/script.js
@@ -1,29 +1,4 @@
-const keyCodesDiv = document.querySelector('.key-codes');
-
-document.addEventListener('keydown', handleKeyPress);
-
-function handleKeyPress({ key }) {
-  const keyDiv = document.createElement('div');
-  keyDiv.innerHTML = getUnicodeCharacter(key);
-  
-  keyCodesDiv.appendChild(keyDiv);
-  keyDiv.className = 'fade-in';
-  keyDiv.dataset.key = key;
-
-  if (keyCodesDiv.children.length > 10) {
-    keyCodesDiv.removeChild(keyCodesDiv.children[0]);
-  }
-
-  const percent = getMatchPercent();
-  if (percent === 1) {
-    document.querySelector('body').classList.add('winner');
-  } else {
-    document.querySelector('body').classList.remove('winner');
-  }
-}
-
-function getMatchPercent() {
-  let konamiCode = ['ArrowUp',
+const konamiCode = ['ArrowUp',
                     'ArrowUp',
                     'ArrowDown',
                     'ArrowDown',
@@ -32,22 +7,42 @@ function getMatchPercent() {
                     'ArrowLeft',
                     'ArrowRight',
                     'b',
-                    'a'].reverse();
+                    'a'];
+const keyCodesDiv = document.querySelector('.key-codes');
+const placeholderDiv = document.querySelector('.placeholder');
+document.addEventListener('keydown', handleKeyPress);
 
-  let keyCodes = [];
-  for (let child of keyCodesDiv.children) {
-    keyCodes.push(child.dataset.key);
+let keyCodes = [];
+function pushKey(key) {
+  keyCodes = [...keyCodes, key];
+  if (keyCodes.length > 10) {
+    [_, ...keyCodes] = keyCodes;
   }
-  keyCodes.reverse();
-  
-  let percent = 0;
-  for (let i=0; i<10; i++) {
-    if (konamiCode[i] === keyCodes[i]) {
-      percent++;
-    }
+}
+
+function handleKeyPress({ key }) {
+  placeholderDiv.classList.add('hidden');
+  pushKey(key);
+
+  const keyDiv = document.createElement('div');
+  keyDiv.innerHTML = getUnicodeCharacter(key);
+  keyCodesDiv.appendChild(keyDiv);
+  keyDiv.className = 'fade-in';
+
+  if (keyCodesDiv.children.length > 10) {
+    keyCodesDiv.removeChild(keyCodesDiv.children[0]);
   }
-  
-  return percent / 10;
+
+  if (isCodeComplete()) {
+    document.querySelector('body').classList.add('winner');
+  } else {
+    document.querySelector('body').classList.remove('winner');
+  }
+}
+
+function isCodeComplete() {
+  return (keyCodes.length === konamiCode.length && 
+          konamiCode.every((v, i) => v === keyCodes[i]));
 }
 
 function getUnicodeCharacter(key) {
